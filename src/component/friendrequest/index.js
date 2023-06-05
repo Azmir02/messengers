3a92e3c0a81fa7cb737632a89270e36d58c59e33
Azmir Uddin Alif <azmir02alif@gmail.com>
2023-06-05 20:01:38 +0600

    some-changes

--- src/component/friendrequest/index.js
+++ src/component/friendrequest/index.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import Button from "@mui/material/Button";
 import "./style.css";
+import { useSelector } from "react-redux";
+import {
+  getDatabase,
+  ref,
+  onValue,
+  set,
+  push,
+  remove,
+} from "firebase/database";
 
 const Friendrequst = () => {
+  const user = useSelector((users) => users.login.loggedIn);
+  const [frndReq, setFrndReq] = useState([]);
+  const db = getDatabase();
+
+  // show friend request
+  useEffect(() => {
+    const starCountRef = ref(db, "friendrequest");
+    onValue(starCountRef, (snapshot) => {
+      let reqArr = [];
+      snapshot.forEach((item) => {
+        if (item.val().reciverid == user.uid) {
+          reqArr.push({ ...item.val(), id: item.key });
+        }
+      });
+      setFrndReq(reqArr);
+    });
+  }, []);
+
+  const handleAcceptreq = (item) => {
+    set(push(ref(db, "friends")), {
+      ...item,
+    }).then(() => {
+      remove(ref(db, "friendrequest/" + item.id));
+    });
+  };
   return (
     <div className="friendrequestlist">
       <div className="friendrequestlist_header">
@@ -13,21 +47,26 @@ const Friendrequst = () => {
           <input type="text" placeholder="Search..." />
         </div>
       </div>
+
       <div className="friendrequestlist_body">
-        <div className="friendrequestlist_wrapper">
-          <div className="friendrequestlist_img">
-            <img src="/assets/avatar.png" alt="avatr" />
+        {frndReq.map((item, i) => (
+          <div className="friendrequestlist_wrapper">
+            <div className="friendrequestlist_img">
+              <img src="/assets/avatar.png" alt="avatr" />
+            </div>
+            <div className="friendrequestlist_titles">
+              <h4>{item.sendername}</h4>
+            </div>
+            <div className="friendrequestlist_join">
+              <Button variant="outlined" onClick={() => handleAcceptreq(item)}>
+                Accept
+              </Button>
+              <Button variant="outlined" color="error">
+                Reject
+              </Button>
+            </div>
           </div>
-          <div className="friendrequestlist_titles">
-            <h4>Shawon islam</h4>
-          </div>
-          <div className="friendrequestlist_join">
-            <Button variant="outlined">Accept</Button>
-            <Button variant="outlined" color="error">
-              Reject
-            </Button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
--- src/component/userlists/index.js
+++ src/component/userlists/index.js
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 import "./style.css";
 import { AiOutlineSearch } from "react-icons/ai";
 import Button from "@mui/material/Button";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue, set, push } from "firebase/database";
 import { useSelector } from "react-redux";
 
 const Userlists = () => {
   const [userLists, setuserLists] = useState([]);
+  const [filterUser, setFilterUser] = useState([]);
+  const [frndReq, setFrndReq] = useState([]);
   const user = useSelector((users) => users.login.loggedIn);
   const db = getDatabase();
 
@@ -22,29 +24,100 @@ const Userlists = () => {
       setuserLists(userArr);
     });
   }, []);
+
+  // show friendreq
+  useEffect(() => {
+    const starCountRef = ref(db, "friendrequest");
+    onValue(starCountRef, (snapshot) => {
+      let frndreqArr = [];
+      snapshot.forEach((item) => {
+        frndreqArr.push(item.val().reciverid + item.val().senderid);
+      });
+      setFrndReq(frndreqArr);
+    });
+  }, []);
+
+  const handleUserSearch = (e) => {
+    let arr = [];
+    if (e.target.value.length == 0) {
+      setFilterUser([]);
+    }
+    userLists.filter((item) => {
+      console.log(item);
+      if (item.username.toLowerCase().includes(e.target.value.toLowerCase())) {
+        arr.push(item);
+        setFilterUser(arr);
+      }
+    });
+  };
+
+  // send frnd req
+  const handleSentFrndreq = (item) => {
+    set(push(ref(db, "friendrequest")), {
+      sendername: user.displayName,
+      senderid: user.uid,
+      recivername: item.username,
+      reciverid: item.id,
+    });
+  };
+
   return (
     <div className="userlists">
       <div className="userlists_header">
         <h2>User's Lists</h2>
         <div className="userlists_searchBoxes">
           <AiOutlineSearch />
-          <input type="text" placeholder="Search..." />
+          <input
+            onChange={handleUserSearch}
+            type="text"
+            placeholder="Search..."
+          />
         </div>
       </div>
       <div className="userlists_body">
-        {userLists.map((item, i) => (
-          <div className="userlists_wrapper" key={i}>
-            <div className="userlists_img">
-              <img src="/assets/avatar.png" alt="avatr" />
-            </div>
-            <div className="userlists_titles">
-              <h4>{item.username}</h4>
-            </div>
-            <div className="userlists_join">
-              <Button variant="outlined">Add friend</Button>
-            </div>
-          </div>
-        ))}
+        {filterUser.length > 0
+          ? filterUser.map((item, i) => (
+              <div className="userlists_wrapper" key={i}>
+                <div className="userlists_img">
+                  <img src="/assets/avatar.png" alt="avatr" />
+                </div>
+                <div className="userlists_titles">
+                  <h4>{item.username}</h4>
+                </div>
+                <div className="userlists_join">
+                  <Button variant="outlined">Add friend</Button>
+                </div>
+              </div>
+            ))
+          : userLists.map((item, i) => (
+              <div className="userlists_wrapper" key={i}>
+                <div className="userlists_img">
+                  <img src="/assets/avatar.png" alt="avatr" />
+                </div>
+                <div className="userlists_titles">
+                  <h4>{item.username}</h4>
+                </div>
+                <div className="userlists_join">
+                  {frndReq.includes(item.id + user.uid) ||
+                  frndReq.includes(user.uid + item.id) ? (
+                    <Button
+                      variant="outlined"
+                      color="error"
+                      className="cancel-req"
+                    >
+                      Cancel Request
+                    </Button>
+                  ) : (
+                    <Button
+                      variant="outlined"
+                      onClick={() => handleSentFrndreq(item)}
+                    >
+                      Add friend
+                    </Button>
+                  )}
+                </div>
+              </div>
+            ))}
       </div>
     </div>
   );
